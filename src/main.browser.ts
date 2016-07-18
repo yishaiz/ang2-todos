a44bd8e752972196c13982d40876405d10891733
yishai <yishai5555@gmail.com>
2016-07-18 17:24:34 +0300

    b4

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -1,7 +1,6 @@
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {TodolistComponent,} from 'components/todolist.component';
 
-bootstrap(TodolistComponent)
-  .catch(err => console.error(err));
+bootstrap(TodolistComponent);
 
 
