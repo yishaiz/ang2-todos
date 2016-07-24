323e277637122fddd8790fd9a8a981c4fa65eda7
yishai <yishai5555@gmail.com>
2016-07-24 15:52:17 +0300

    b13

--- src/components/item.component.ts
+++ src/components/item.component.ts
@@ -11,7 +11,101 @@ import {Component, Input, ViewEncapsulation} from '@angular/core';
 @Component({
   selector: 'todo-item',
   // inputs:[task],
-  styles: [],
+  styles: [`
+
+button {
+/*background-color:red;*/
+
+	margin: 0;
+	padding: 0;
+	border: 0;
+	background: none;
+	font-size: 100%;
+	vertical-align: baseline;
+	font-family: inherit;
+	font-weight: inherit;
+	color: inherit;
+	-webkit-appearance: none;
+	appearance: none;
+	-webkit-font-smoothing: antialiased;
+	-moz-font-smoothing: antialiased;
+	font-smoothing: antialiased;
+}
+
+ 
+
+button,
+input[type="checkbox"] {
+	outline: none;
+}
+
+
+
+label {
+ 	white-space: pre-line;
+	word-break: break-all;
+	padding: 15px 60px 15px 15px;
+	margin-left: 45px;
+	display: block;
+	line-height: 1.2;
+	transition: color 0.4s;
+}
+
+.todo-list li.completed label {
+	color: #d9d9d9;
+	text-decoration: line-through;
+}
+
+ .destroy {
+	display: none;
+	position: absolute;
+	top: 0;
+	right: 10px;
+	bottom: 0;
+	width: 40px;
+	height: 40px;
+	margin: auto 0;
+	font-size: 30px;
+	color: #cc9a9a;
+	margin-bottom: 11px;
+	transition: color 0.2s ease-out;
+}
+
+  .destroy:hover {
+	color: #af5b5e;
+}
+
+ .destroy:after {
+	content: '×';
+}
+
+.todo-list li:hover .destroy {
+	display: block;
+}
+
+ input::-webkit-input-placeholder {
+	font-style: italic;
+	font-weight: 300;
+	color: #e6e6e6;
+}
+
+  input::-moz-placeholder {
+	font-style: italic;
+	font-weight: 300;
+	color: #e6e6e6;
+}
+
+  input::input-placeholder {
+	font-style: italic;
+	font-weight: 300;
+	color: #e6e6e6;
+}
+
+
+/*label{color:green}*/
+
+/*button{}*/
+`],
   directives: [],
   template: `
     
@@ -29,8 +123,7 @@ import {Component, Input, ViewEncapsulation} from '@angular/core';
              [checked]="task.checked">
      
     `
-  ,
-  encapsulation: ViewEncapsulation.None
+  // ,   encapsulation: ViewEncapsulation.None
 
 })
 
--- src/components/todolist-footer.component.ts
+++ src/components/todolist-footer.component.ts
@@ -2,7 +2,55 @@ import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'todo-list-footer',
+styles:[`
 
+button {
+/*background-color:red;*/
+
+	margin: 0;
+	padding: 0;
+	border: 0;
+	background: none;
+	font-size: 100%;
+	vertical-align: baseline;
+	font-family: inherit;
+	font-weight: inherit;
+	color: inherit;
+	-webkit-appearance: none;
+	appearance: none;
+	-webkit-font-smoothing: antialiased;
+	-moz-font-smoothing: antialiased;
+	font-smoothing: antialiased;
+}
+
+
+
+.footer {
+	color: #777;
+	padding: 10px 15px;
+	height: 20px;
+	text-align: center;
+	border-top: 1px solid #e6e6e6;
+}
+
+.footer:before {
+	content: '';
+	position: absolute;
+	right: 0;
+	bottom: 0;
+	left: 0;
+	height: 50px;
+	overflow: hidden;
+	box-shadow: 0 1px 1px rgba(0, 0, 0, 0.2),
+	            0 8px 0 -3px #f6f6f6,
+	            0 9px 1px -3px rgba(0, 0, 0, 0.2),
+	            0 16px 0 -6px #f6f6f6,
+	            0 17px 2px -6px rgba(0, 0, 0, 0.2);
+}
+
+
+
+`],
   template: `
     
 
@@ -15,8 +63,7 @@ import {Component, ViewEncapsulation} from '@angular/core';
     <button class="clear-completed">Clear completed</button>
   </footer>
   <!--End Footer-->  `
-  ,
-  encapsulation: ViewEncapsulation.None
+  ,  encapsulation: ViewEncapsulation.None
 
 })
 
